0df7ab46f5978fa2ccfae278af30de8c087b432b
Grisha55 <grishavinyar@icloud.com>
2026-03-21 14:57:56 +0300

    Made LoadingScreen for loading ui

--- src/shared/config/routeConfig/AppRouter.tsx
+++ src/shared/config/routeConfig/AppRouter.tsx
@@ -1,36 +1,43 @@
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { useAuth } from '../../../app/providers/auth-provider';
+import { LoadingScreen } from '../../ui/LoadingScreen';
+import { ProtectedRoute } from './ProtectedRoute';
 import { routeConfig } from './routeConfig';
-import { ProtectedRoute } from './ProtectedRoute'
-import { useAuth } from '../../../app/providers/auth-provider'
 
 export const AppRouter = () => {
-  const { user, loading } = useAuth();
+	const { user, loading } = useAuth();
 	console.log('AppRouter - user: ', user, 'loading: ', loading);
 
-  if (loading) {
-    return <div>Loading...</div>
-  }
+	if (loading) {
+		return <LoadingScreen />;
+	}
 
-  return (
-    <Routes>
-      {Object.values(routeConfig).map(({ path, component: Component }) => {
-        // Если это login или registration — не защищаем
-        if (path === '/login' || path === '/registration') {
-          return <Route key={path} path={path} element={<Component />} />;
-        }
+	return (
+		<Routes>
+			{Object.values(routeConfig).map(({ path, component: Component }) => {
+				// Если это login или registration — не защищаем
+				if (path === '/login' || path === '/registration') {
+					return (
+						<Route
+							key={path}
+							path={path}
+							element={<Component />}
+						/>
+					);
+				}
 
-        return (
-          <Route
-            key={path}
-            path={path}
-            element={
-              <ProtectedRoute>
-                <Component />
-              </ProtectedRoute>
-            }
-          />
-        );
-      })}
-    </Routes>
-  );
+				return (
+					<Route
+						key={path}
+						path={path}
+						element={
+							<ProtectedRoute>
+								<Component />
+							</ProtectedRoute>
+						}
+					/>
+				);
+			})}
+		</Routes>
+	);
 };
